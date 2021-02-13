bce67bf95f359eae227550082b51253f4294adf0
dhirdi <67578591+dhirdi@users.noreply.github.com>
2021-02-13 15:33:09 +0700

    Add files via upload

---
+++ pertamakali.py
@@ -0,0 +1,26 @@
+'''
+Cara menampilkan text di layar
+'''
+
+print("Kelas Basic Phyton!")
+
+'''
+Contoh variabel
+'''
+x = 5
+y = "Hello World"
+ini_variabel = "Bener kan variabel"
+a,b,c = 10,20,30
+print(x) 
+print(y)
+print(ini_variabel)
+print(a)
+print(b)
+print(c)
+
+
+#nampilinn tipe data
+
+print(type(y))
+print(type(a))
+
